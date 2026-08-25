c2752a72b2ee58574dd5678dfd5649833e32ffc9
Nikolay <nikolai.mironoff2013@gmail.com>
2026-08-25 14:01:58 +0100

    Add `--performance` arg into run.ps1

--- run.ps1
+++ run.ps1
@@ -1,1 +1,1 @@
-python crack_runner.py seed.json file.rar.hash -m 13000 --prefix out --dictgen-args "--min-length 12 --max-length 32 --split-count 1000000"
+python crack_runner.py seed.json file.rar.hash -m 13000 --prefix out --performance --dictgen-args "--min-length 12 --max-length 32 --split-count 1000000"
